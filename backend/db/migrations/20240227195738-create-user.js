a565345bbe110c97b002103f4f02d5268f7cc308
Marisela Gomez <mariselaalejandragomez@gmail.com>
2024-03-01 09:24:47 -0800

    lil user migration change

--- backend/db/migrations/20240227195738-create-user.js
+++ backend/db/migrations/20240227195738-create-user.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === "production") {
 
 module.exports = {
 	async up(queryInterface, Sequelize) {
-		const result = await queryInterface.createTable(
+		await queryInterface.createTable(
 			"Users",
 			{
 				id: {
